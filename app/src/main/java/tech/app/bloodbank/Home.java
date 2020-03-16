598289c72bbd2b9453b5d31cc9ae1fdc0183b98a
Kazi1318914 <Iammahin1318914>
2020-03-16 10:49:57 +0600

    slideview

--- app/src/main/java/tech/app/bloodbank/Home.java
+++ app/src/main/java/tech/app/bloodbank/Home.java
@@ -3,12 +3,34 @@ package tech.app.bloodbank;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class Home extends AppCompatActivity {
+
+    int imageArr[] = {R.drawable.bgo, R.drawable.bgt, R.drawable.bgth};
+    ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        iv = findViewById(R.id.sv_iv);
+
+        Runnable r = new  Runnable(){
+            int i=0;
+            public void run()
+            {
+                iv.setImageResource(imageArr[i]);
+                i++;
+                if(i > imageArr.length-1)
+                {
+                    i = 0;
+                }
+                iv.postDelayed(this,5000);
+
+            }
+        };
+        iv.postDelayed(r,5000);
     }
 }
