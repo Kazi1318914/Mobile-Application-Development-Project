238b671fe40535466041029e6f3c031fca5c3796
Kazi1318914 <Iammahin1318914>
2020-03-23 19:50:38 +0600

    slideview, no scrollbar, add pics, homechange

--- app/src/main/java/tech/app/bloodbank/Home.java
+++ app/src/main/java/tech/app/bloodbank/Home.java
@@ -2,6 +2,7 @@ package tech.app.bloodbank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -10,7 +11,7 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
-public class Home extends AppCompatActivity {
+public class Home extends AppCompatActivity implements  View.OnClickListener{
 
     int imageArr[] = {R.drawable.bgo, R.drawable.bgt, R.drawable.bgth};
     ImageView iv;
@@ -44,11 +45,57 @@ public class Home extends AppCompatActivity {
             for (int j = 0; j < 3; j++) {
                 String imgID = "iv"+i+j;
                 String txtID = "tv"+i+j;
+                pImageViews[i][j] = findViewById(getResources().getIdentifier(imgID, "id", getPackageName()));
+                pTextViews[i][j] = findViewById(getResources().getIdentifier(txtID, "id", getPackageName()));
+
+                pImageViews[i][j].setOnClickListener(this);
+                pTextViews[i][j].setOnClickListener(this);
             }
         }
     }
 
     public void test(View view) {
         Toast.makeText(this,"LayoutClickableDone",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onClick(View v) {
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                    if(v == pImageViews[i][j] || v == pTextViews[i][j])
+                    {
+                        if(i == 0 && j == 0)
+                        {
+                            Intent it = new Intent(this, RequestForBlood.class);
+                            startActivity(it);
+                        }
+                        else if(i == 0 && j == 1)
+                        {
+                            Toast.makeText(this, "Feed", Toast.LENGTH_SHORT).show();
+
+                        }
+                        else if(i == 0 && j == 2)
+                        {
+                            Toast.makeText(this, "Organization", Toast.LENGTH_SHORT).show();
+
+                        }
+                        else if(i == 1 && j == 0)
+                        {
+                            Toast.makeText(this, "Ambulance", Toast.LENGTH_SHORT).show();
+
+                        }
+                        else if(i == 1 && j == 1)
+                        {
+                            Toast.makeText(this, "I Sleep", Toast.LENGTH_SHORT).show();
+
+                        }
+                        else if(i == 1 && j == 2)
+                        {
+                            Toast.makeText(this, "Info", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                }
+            }
     }
 }
