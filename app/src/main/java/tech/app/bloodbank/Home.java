6f42c7e55efb6a07b0330ece01cb285ff7cc226b
Kazi1318914 <Iammahin1318914>
2020-03-23 17:24:05 +0600

    slideview, no scrollbar, add pics, homechange

--- app/src/main/java/tech/app/bloodbank/Home.java
+++ app/src/main/java/tech/app/bloodbank/Home.java
@@ -6,11 +6,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class Home extends AppCompatActivity {
 
     int imageArr[] = {R.drawable.bgo, R.drawable.bgt, R.drawable.bgth};
     ImageView iv;
+    ImageView[][] pImageViews = new ImageView[2][3];
+    TextView[][] pTextViews = new TextView[2][3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +34,18 @@ public class Home extends AppCompatActivity {
                 {
                     i = 0;
                 }
-                iv.postDelayed(this,5000);
+                iv.postDelayed(this,5000);  //rest of the delay
 
             }
         };
-        iv.postDelayed(r,5000);
+        iv.postDelayed(r,5000);                     //for the 1st delay
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                String imgID = "iv"+i+j;
+                String txtID = "tv"+i+j;
+            }
+        }
     }
 
     public void test(View view) {
